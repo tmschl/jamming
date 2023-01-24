bb9483a837c944aeb11fc1f12301e776d2c9c050
tmschl <timothy.schiller@gmail.com>
2023-01-24 08:59:25 -0500

    creates component files, begins to mimic spotify api call

---
+++ src/Components/App/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import {SearchBar} from "../SearchBar/SearchBar";
+import {SearchResults} from "../SearchResults/SearchResults";
+import {Playlist} from "../Playlist/Playlist";
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [{
+        name: '',
+        artist: '',
+        album: '',
+        id: null
+      },{
+        name: '',
+        artist: '',
+        album: '',
+        id: null
+      }]
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          < SearchBar searchResults={this.state.searchResults}/>
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+
+  }
+}
+
+export default App;
