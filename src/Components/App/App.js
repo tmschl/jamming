0e2b12d33d6c90020a6e9c788a458d29b5f6e46d
tmschl <timothy.schiller@gmail.com>
2023-01-25 10:22:13 -0500

    app method to handle search state updates. searchbar component method to handle search and update state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -47,6 +47,9 @@ export class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+
   }
 
   addTrack(track) {
@@ -63,18 +66,26 @@ export class App extends React.Component {
     this.setState({playlistName: name})
   }
 
-  removeTrack (track) {
+  removeTrack(track) {
     let tracks = this.state.playlistTracks;
     tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
     this.setState({playlistTracks: tracks})
   }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+   }
+
+   search(term) {
+    console.log(term);
+   }
 
   render () {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults  searchResults={this.state.searchResults}
                             onAdd={this.addTrack}/>
@@ -82,7 +93,8 @@ export class App extends React.Component {
                       playlistTracks={this.state.playlistTracks} 
                       searchResults={this.state.searchResults}
                       onRemove={this.removeTrack}
-                      onNameChange={this.updatePlaylistName} />
+                      onNameChange={this.updatePlaylistName} 
+                      onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -22,7 +22,7 @@ export class Playlist extends React.Component {
           <TrackList tracks={this.props.playlistTracks} 
                       onRemove={this.props.onRemove} 
                       isRemoval={true}/>
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <button onClick={this.props.onSave} className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
 
     )
